fa265d927abd95bf3f7c587592ec527ec413393d
Ruslan <medtehnik850@gmail.com>
2022-02-26 19:59:46 +0700

    Task1: Part2

---
+++ src/com/satybaev/homework/task1/Name.java
@@ -0,0 +1,17 @@
+package com.satybaev.homework.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Name {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please write your name and age");
+        String name = reader.readLine();
+        String age = reader.readLine();
+        int nAge = Integer.parseInt(age);
+
+        System.out.println("Your name is: " + name + "\n" + "Your age: " + nAge);
+    }
+}
