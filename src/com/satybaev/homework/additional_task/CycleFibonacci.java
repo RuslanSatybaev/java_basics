79499f60284487ec72220c0de9fe786f1bfad1eb
Ruslan <medtehnik850@gmail.com>
2022-02-26 20:40:04 +0700

    Additional Task: Number Fibonacci

---
+++ src/com/satybaev/homework/additional_task/CycleFibonacci.java
@@ -0,0 +1,20 @@
+package com.satybaev.homework.additional_task;
+
+public class CycleFibonacci {
+    public static void main(String[] args) {
+        int number0 = 0;
+        int numberFirst = 1;
+        int numberSecond = 1;
+
+        int numberFibonacci;
+        System.out.print(number0 + " " + numberFirst + " " + numberSecond + " ");
+
+        for (int i = 3; i < 20; i++) {
+            numberFibonacci = numberFirst + numberSecond;
+            System.out.print(numberFibonacci + " ");
+            numberFirst = numberSecond;
+            numberSecond = numberFibonacci;
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/satybaev/homework/additional_task/RecursiveFibonacci.java
@@ -0,0 +1,22 @@
+package com.satybaev.homework.additional_task;
+
+public class RecursiveFibonacci {
+    //f(n)=f(n-1) + f(n-2), причем f(0)=0, а f(1)=1
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    static int fibonacci(int n){
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
